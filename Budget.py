a653f25c2315923f7a99174cef12331b9fb52169
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-01 23:08:38 -0600

    init

--- Budget.py
+++ Budget.py
@@ -10,11 +10,9 @@ st.title('Presupuesto 2023 Maldonado Chaves SA de CV')
 with st.container():
     with st.expander("Presupuesto 2023"):
         path= r"C:\Users\XPC\OneDrive\Desktop\StreamlitBudget"+'\\'
-        #Budget2023 = pd.read_csv(path+'Budget.csv', encoding='latin-1')
-        st.write(path+'Budget.csv')
-
-
-
+        Budget2023 = pd.read_csv(path+'Budget.csv', encoding='latin-1')
+        st.dataframe(Budget2023, 200, 100)
+        
     with st.expander("Ingresos"):
         st.write("Holi")
     with st.expander("Gastos"):
