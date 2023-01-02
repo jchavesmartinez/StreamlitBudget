7bb46c76fd2504d360addf17b5147fe6ed8ebf69
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-01 23:00:51 -0600

    init

--- Budget.py
+++ Budget.py
@@ -8,11 +8,10 @@ st.title('Presupuesto 2023 Maldonado Chaves SA de CV')
 
 with st.container():
     with st.expander("Presupuesto 2023"):
-        df = pd.DataFrame(
-        np.random.randn(50, 20),
-        columns=('col %d' % i for i in range(20)))
 
-        st.dataframe(df)  # Same as st.write(df)
+        
+
+
     with st.expander("Ingresos"):
         st.write("Holi")
     with st.expander("Gastos"):
