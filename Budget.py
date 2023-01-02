f8ada7cbd8bd94033cadbd471b365f83a9b1293b
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-01 22:43:04 -0600

    init

--- Budget.py
+++ Budget.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title('Presupuesto 2023 Maldonado Chaves SA de CV')
+
+import streamlit as st
+
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
+
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
