5047ee2a374239869f0d2123f9a95b2af64f2e2a
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-02 01:03:41 -0600

    init

--- Budget.py
+++ Budget.py
@@ -56,7 +56,7 @@ with st.expander("Metricas y resultados"):
     elif quicena_option=='Segunda quincena':
         fechafiltrar= mes_option + ' II'
     
-    DiarioFinal=Diario[Diario['Fecha']==str(fechafiltrar)]
+    DiarioFinal=Diario[Diario['Fecha'].str.match(fechafiltrar)]
     DiarioFinal = DiarioFinal.groupby(['Fecha','Motivo'])['Monto'].sum()
 
 
