b2d1387853ec1803f9c6ebcd0fb7952c3f54e72a
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-01 22:53:47 -0600

    init

--- Budget.py
+++ Budget.py
@@ -4,6 +4,10 @@ import time
 st.title('Presupuesto 2023 Maldonado Chaves SA de CV')
 
 with st.container():
-    with st.expander("See explanation"):
+    with st.expander("Presupuesto 2023"):
+        st.write("Holi")
+    with st.expander("Ingresos"):
+        st.write("Holi")
+    with st.expander("Gastos"):
         st.write("Holi")
 
