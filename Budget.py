7d252cb54afdb0d22b14bec4c750ecc40dc89d7b
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-01 22:55:02 -0600

    init

--- Budget.py
+++ Budget.py
@@ -1,11 +1,18 @@
 import streamlit as st
-import time 
+import time
+import pandas as pd
+import numpy as np
+
 
 st.title('Presupuesto 2023 Maldonado Chaves SA de CV')
 
 with st.container():
     with st.expander("Presupuesto 2023"):
-        st.write("Holi")
+        df = pd.DataFrame(
+        np.random.randn(50, 20),
+        columns=('col %d' % i for i in range(20)))
+
+        st.dataframe(df)  # Same as st.write(df)
     with st.expander("Ingresos"):
         st.write("Holi")
     with st.expander("Gastos"):
