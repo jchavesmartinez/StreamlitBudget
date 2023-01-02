cb575eaa89b9698009d97949a3600fcb742e375c
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-01 23:56:15 -0600

    init

--- Budget.py
+++ Budget.py
@@ -32,15 +32,15 @@ with st.expander("Ingresos"):
 
 with st.expander("Gastos"):
     if st.checkbox("Registrar gasto"):
-        with st.form("my_form"):
-            motivo_option = st.selectbox('Motivo',('Salario Jose', 'Salario Aline', 'Ingreso extra'))
-            monto_ingreso = st.number_input('Monto')
-            currency_option = st.selectbox('Moneda',('Colones', 'Pesos', 'USD'))
-            nota_input = st.text_input('Comentario')
+        with st.form("my_form2"):
+            motivo_option2 = st.selectbox('Motivo',('Salario Jose', 'Salario Aline', 'Ingreso extra'))
+            monto_ingreso2 = st.number_input('Monto')
+            currency_option2 = st.selectbox('Moneda',('Colones', 'Pesos', 'USD'))
+            nota_input2 = st.text_input('Comentario')
             
-            submitted = st.form_submit_button("Submit")
+            submitted2 = st.form_submit_button("Submit")
         
-        if submitted:
+        if submitted2:
             st.write("slider")
 
 with st.expander("Metricas y resultados"):
