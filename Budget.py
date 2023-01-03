4eb802016c1dd1d3452ed988e660c0adbe82e9d7
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-03 09:13:43 -0600

    init

--- Budget.py
+++ Budget.py
@@ -134,16 +134,17 @@ with st.expander("Metricas y resultados"):
 
     DiarioFinal1['Saldo Consumido %'] = 100 - (DiarioFinal1['Saldo Disponible']/DiarioFinal1['Saldo Presupuestado']*100)
 
-    col1, col2, col3, col4,col5 = st.columns([1,1,1,1,1])
-    #col1.metric("Temperature", "70 °F", "1.2 °F")
-    col2.metric("Saldo Presupuestado", "9 mph", "-8%")
-    col3.metric("Saldo Consumido", "86%", "4%")
-    col4.metric("Saldo Disponible", "86%", "4%")
-    col5.metric("Saldo Consumido", "86%", "4%")
+
 
     tab1, tab2= st.tabs(["Metricas", "Resumen"])
 
     with tab1:
+        col1, col2, col3, col4,col5 = st.columns([1,1,1,1,1])
+        #col1.metric("Temperature", "70 °F", "1.2 °F")
+        col2.metric("Saldo Presupuestado", "9 mph", "-8%")
+        col3.metric("Saldo Consumido", "86%", "4%")
+        col4.metric("Saldo Disponible", "86%", "4%")
+        col5.metric("Saldo Consumido", "86%", "4%")
         st.dataframe(DiarioFinal1,use_container_width=True)
 
     with tab2:
