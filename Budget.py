61e6b9f97ce466f68d193397c21d2738e0a8e1b6
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-03 16:39:55 -0600

    init

--- Budget.py
+++ Budget.py
@@ -12,6 +12,7 @@ import json
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
