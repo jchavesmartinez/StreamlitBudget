0eff0c1f64d4a3a3ecafa70e19ffcf9e875bf485
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-04 16:05:52 -0600

    init

--- Budget.py
+++ Budget.py
@@ -255,7 +255,7 @@ with st.expander("Saldos"):
             if st.checkbox("Pagar tarjeta Aline"):
                 
                 st.write("Hola")
-                with st.form("my_form22",key="PagoAline"):
+                with st.form("PagoAline"):
                     cuenta_option22 = st.selectbox('Cuenta Bancaria',('Tarjeta debito Jose', 'Tarjeta debito Aline'),key='pagoaline1')            
                 #    monto_ingreso2 = st.number_input('Monto',key='pagoaline2')
                 #   submitted4 = st.form_submit_button("Confirmar pago",key='pagoaline3')
