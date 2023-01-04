aef7537868419028f2eae7f61f9dfb1e020a1458
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-03 19:59:00 -0600

    init

--- Budget.py
+++ Budget.py
@@ -230,7 +230,7 @@ with st.expander("Saldos"):
             debitototal=DiarioFinal2['Monto'].sum()
             col7, col8, col9, col10,col11 = st.columns([1,1.2,1,1,1])
             col9.metric("Saldo Credito",abs(debitototal)+saldoinicial, "%")
-            col11.button('Say hello')
+            col11.button('Pagar tarjeta credito Jose')
 
         elif cuenta_option2=='Tarjeta credito Aline':
             DiarioFinal2=DiarioFinal2[DiarioFinal2['Cuenta']==cuenta_option2]
@@ -239,6 +239,7 @@ with st.expander("Saldos"):
             debitototal=DiarioFinal2['Monto'].sum()
             col7, col8, col9, col10,col11 = st.columns([1,1.2,1,1,1])
             col9.metric("Saldo Credito",abs(debitototal)+saldoinicial, "%")
+            col11.button('Pagar tarjeta credito Aline')
 
         
         st.dataframe(DiarioFinal2,use_container_width=True)
