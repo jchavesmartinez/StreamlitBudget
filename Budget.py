57809c43001042eec2793afdb43f0c7fb5d0453f
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-03 23:28:46 -0600

    init

--- Budget.py
+++ Budget.py
@@ -235,9 +235,9 @@ with st.expander("Saldos"):
                 with st.form("my_form2"):
                     cuenta_option2 = st.selectbox('Cuenta Bancaria',('Tarjeta debito Jose', 'Tarjeta debito Aline'))            
                     monto_ingreso2 = st.number_input('Monto')
-                    submitted2 = st.form_submit_button("Confirmar pago")
+                    submitted3 = st.form_submit_button("Confirmar pago")
                 
-                if submitted2:
+                if submitted3:
                     #db.COLLECTION_DIARIO.insert_one({"_id": len(Diario)+1, "Motivo": 'Tasa 0', "Tipo": 'Gasto', "Monto": monto_ingreso2, "Fecha": fecha, "Escenario": '2. Actual', "Cuenta": 'Tarjeta credito Jose', "Nota": 'Pago tarjeta de credito Jose'})
                     #db.COLLECTION_DIARIO.insert_one({"_id": len(Diario)+2, "Motivo": 'Tasa 0', "Tipo": 'Gasto', "Monto": -monto_ingreso2, "Fecha": fecha, "Escenario": '2. Actual', "Cuenta": cuenta_option2, "Nota": 'Pago tarjeta de credito Jose'})
                     st.success('This is a success message!!', icon="✅")
