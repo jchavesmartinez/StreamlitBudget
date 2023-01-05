4b9f4412d51d201580708da2e5dec74bbd2986f9
jchavesmartinez <jchavesm2017@gmail.com>
2023-01-05 16:39:05 -0600

    init

--- Budget.py
+++ Budget.py
@@ -154,7 +154,7 @@ with st.expander("Metricas y resultados"):
     DiarioCalculosA=DiarioFinalActual1[(DiarioFinalActual1.Motivo != "Salario")]
     DiarioCalculosA=DiarioCalculosA[(DiarioCalculosA.Motivo != "Ingresos Extra")]
 
-    Gastos_faltantes=DiarioFinal1[DiarioFinal1['Saldo Disponible'] > 0]
+    Gastos_faltantes=DiarioFinal1[DiarioFinal1['Saldo Disponible'] < 0]
     Gastos_faltantes1=Gastos_faltantes['Saldo Disponible'].sum()
 
     tab1, tab2= st.tabs(["Metricas", "Resumen"])
