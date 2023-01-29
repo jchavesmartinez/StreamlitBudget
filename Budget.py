1af4dd4f938253d4e46203df8dae01517e997081
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-01-28 21:30:22 -0600

    Update Budget.py

--- Budget.py
+++ Budget.py
@@ -252,7 +252,7 @@ with st.expander("Saldos"):
             saldoinicial=-(322862.82+176914.08)
             debitototal=DiarioFinal2['Monto'].sum()
             col27, col28, col29, col210,col211 = st.columns([1,1.2,1,1,1])
-            col29.metric("Saldo Credito",abs(debitototal+saldoinicial), "%")
+            col29.metric("Saldo Credito",int(abs(debitototal+saldoinicial)), "%")
 
         if cuenta_option2=='Tarjeta credito Jose':
             DiarioFinal2=DiarioFinal2[DiarioFinal2['Cuenta']==cuenta_option2]
@@ -260,7 +260,7 @@ with st.expander("Saldos"):
 
             debitototal=DiarioFinal2['Monto'].sum()
             col17, col18, col19, col110,col111 = st.columns([1,1.2,1,1,1])
-            col19.metric("Saldo Credito",abs(debitototal+saldoinicial), "%")
+            col19.metric("Saldo Credito",int(abs(debitototal+saldoinicial)), "%")
 
             if st.checkbox("Pagar tarjeta Jose"):
                 
