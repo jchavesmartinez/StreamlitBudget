138a9e0f4a6d1a66f20b5a88bbaf7b4114398af9
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-04-04 11:31:55 -0600

    Update Budget.py

--- Budget.py
+++ Budget.py
@@ -279,14 +279,14 @@ with st.expander("Saldos"):
         DiarioFinal2=DiarioFinal2[DiarioFinal2['Cuenta'].str.contains('credito')]
 
         if cuenta_option2=='Ambas':
-            saldoinicial=-(162233.9+12051)
+            saldoinicial=-(162233.9+12051+226971.96)
             debitototal=DiarioFinal2['Monto'].sum()
             col27, col28, col29, col210,col211 = st.columns([1,1.2,1,1,1])
             col29.metric("Saldo Credito",int(abs(debitototal+saldoinicial)), "%")
 
         if cuenta_option2=='Tarjeta credito Jose':
             DiarioFinal2=DiarioFinal2[DiarioFinal2['Cuenta']==cuenta_option2]
-            saldoinicial=-(162233.9+12051)
+            saldoinicial=-(162233.9+12051+226971.96)
 
             debitototal=DiarioFinal2['Monto'].sum()
             col17, col18, col19, col110,col111 = st.columns([1,1.2,1,1,1])
