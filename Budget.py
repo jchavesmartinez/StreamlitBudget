da409043fc6ea8b000ac95415c0ed84f3f735536
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-04-05 18:29:14 -0600

    init

--- Budget.py
+++ Budget.py
@@ -34,8 +34,8 @@ PASSWORD = "Jccm130199!"
 db = harperdb.HarperDB(url=URL, username=USERNAME, password=PASSWORD)
 SCHEMA= str('PRESUPUESTO_FAMILIAR')
 TABLE1= str('DIARIO')
-libro_precios=pd.DataFrame(db.sql("SELECT * FROM {0}.{1}".format(SCHEMA,TABLE1)))
-libro_precios = libro_precios[['_id','Motivo','Tipo','Monto','Fecha','Escenario','Cuenta','Nota']]
+Diario=pd.DataFrame(db.sql("SELECT * FROM {0}.{1}".format(SCHEMA,TABLE1)))
+Diario = Diario[['_id','Motivo','Tipo','Monto','Fecha','Escenario','Cuenta','Nota']]
 
 path= "https://raw.githubusercontent.com/jchavesmartinez/StreamlitBudget/main/Budget.csv"
 Budget2023 = pd.read_csv(path, encoding='latin-1',index_col=0)
@@ -50,11 +50,9 @@ client = pymongo.MongoClient(CONNECTION_STRING)
 db = client[DB_NAME]
 cursor = db.COLLECTION_DIARIO # choosing the collection you need
 
-Diario = libro_precios
-
 Diario2=Diario[Diario['Fecha'].str.contains('Enero')]
 
-st.dataframe(Diario2)
+st.dataframe(Diario)
 
 
 
